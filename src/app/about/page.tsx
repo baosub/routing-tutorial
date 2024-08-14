a805c093e86bcb28a86bd66c66c0dc29e097cd87
Baldo <barrosoub@protonmail.com>
2024-08-13 18:26:29 -0600

    a simple route was added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Home Page'
+  title: 'About Page'
 };
 
 const page = () => {
   return (
     <div>
-      This is home page
+        about
       
     </div>
   )
